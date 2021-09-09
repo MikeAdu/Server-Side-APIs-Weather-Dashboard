6aeb98c2ff146c5119446c83e9d1d2629c041faa
MikeAdu <mike_adusah@yahoo.com>
2021-09-09 15:45:38 -0700

    created a function for the UVindex

--- app.js
+++ app.js
@@ -70,3 +70,14 @@ function todaysWeather(city){
 
     });
 }
+
+function UVIndex(ln,lt){
+    var uvqURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey+"&lat="+lt+"&lon="+ln;
+    $.ajax({
+            url:uvqURL,
+            method:"GET"
+            }).then(function(response){
+                $(currentUvindex).html(response.value);
+            });
+}
+
