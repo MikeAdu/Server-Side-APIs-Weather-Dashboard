53dd75b6e95eae94f5c54fbe65991b5f8f88f6af
MikeAdu <mike_adusah@yahoo.com>
2021-09-09 15:01:48 -0700

    declared vars

---
+++ app.js
@@ -0,0 +1,11 @@
+var city="";
+
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
